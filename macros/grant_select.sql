d151e64d97051829cb16ca44dde33b0fefa7eb98
amit sonagra <eamite@gmail.com>
2022-07-23 01:21:32 +0000

    grant macro

---
+++ macros/grant_select.sql
@@ -0,0 +1,15 @@
+{% macro grant_select(schema=target.schema,role=target.role)%}
+
+{% set query %}
+grant usage on schema {{schema}} to role {{role}};
+grant select on all tables in schema {{schema}} to role {{role}};
+grant select on all views in schema {{schema}} to role {{role}};
+{% endset%}
+
+{{LOG("GRANTING ACCESS TO TABLES AND VIEWS"~ schema "to role" ~ role,info=true )}}
+
+{% do run_query(query) %}
+
+{{log("granted access", info =true)}}
+
+{% endmacro %}
